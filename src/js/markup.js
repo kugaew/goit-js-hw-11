9d801656aadc48704e4f8263068322dd4dff45b3
Serhii Kuhaiev <kugaew@gmail.com>
2022-10-31 18:34:42 +0100

    add simple scroll when images are download

--- src/js/markup.js
+++ src/js/markup.js
@@ -6,7 +6,20 @@ const refs = new Refs();
 
 export function makeMarkup(data) {
   const { images, totalHits, page, perPage } = data;
+
   refs.gallery.insertAdjacentHTML('beforeend', markupImages(images));
+
+  if (page > 1) {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 1,
+      behavior: 'smooth',
+    });
+  }
+
   if (!isNotEnd(totalHits, page, perPage)) {
     Notify.info("We're sorry, but you've reached the end of search results.");
     refs.loadMoreBtn.setAttribute('disabled', 'true');
